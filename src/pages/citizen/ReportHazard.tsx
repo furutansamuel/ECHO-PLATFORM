1a635eae35048b1fa02fe624f686451bed577f23
Furutan Samuel <furutansamuel@gmail.com>
2026-07-10 18:54:11 +0100

    Update ReportHazard.tsx

--- src/pages/citizen/ReportHazard.tsx
+++ src/pages/citizen/ReportHazard.tsx
@@ -3,7 +3,7 @@ import ReportWizard from '@/components/reports/ReportWizard';
 
 export default function ReportHazard() {
   return (
-    <div className="min-h-screen bg-background pb-20">
+    <div className="min-h-[100dvh] bg-background pb-20">
       <ReportWizard />
     </div>
   );
